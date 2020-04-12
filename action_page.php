ea23123dbd51b2acd5834d432815370c52c76c53
fagand <47316903+fagand@users.noreply.github.com>
2020-04-12 11:36:56 +0100

    UX + added recaptcha to contact form
    Added scroll function to ux when clicking links in the intro.
    Added a recaptcha to contact form to reduce spam submissions.
    + Captcha error & thankyou pages

--- action_page.php
+++ action_page.php
@@ -1,61 +1,48 @@
 <?php
-if(!isset($_POST['submit']))
-{
-	//This page should not be accessed directly. Need to submit the form.
-	echo "error; you need to submit the form!";
-}
+$public_key = "6LflpOgUAAAAANkiX_QNPD7xz8fPmDIUGehCmwtO";
+/* Your reCaptcha public key */
+$private_key = "6LflpOgUAAAAAFhxizTWcjxKQwkO_2mYLIlXg3yY";
+/* Enter your reCaptcha private key */
+$url = "https://www.google.com/recaptcha/api/siteverify";
+/* Default end-point, please verify this before using it */
 $name = $_POST['Name'];
 $visitor_email = $_POST['Email'];
 $message = $_POST['Message'];
-
-//Validate first
-if(empty($name)||empty($visitor_email)) 
+$captcha;
+if(isset($_POST['g-recaptcha-response'])){
+          $captcha=$_POST['g-recaptcha-response'];
+        }
+        if(!$captcha){
+        header('Location: captcha.html');
+          exit;
+        }
+if (array_key_exists('submit', $_POST))
 {
-    echo "Name and email are mandatory!";
-    exit;
-}
-
-if(IsInjected($visitor_email))
-{
-    echo "Bad email value!";
-    exit;
-}
-
-$email_from = "$visitor_email";//<== update the email address
-$email_subject = "New website submission";
-$email_body = "You have received a new message from $name.\n".
-    "Here is the message: $message";
-    
-$to = "admin@davidfagan.co.uk";//<== update the email address
-$headers = "From: $email_from \r\n";
-$headers .= "Reply-To: $visitor_email \r\n";
-//Send the email!
-mail($to,$email_subject,$email_body,$headers);
-//done. redirect to thank-you page.
-header('Location: thank-you.html');
-
-
-// Function to validate against any email injection attempts
-function IsInjected($str)
-{
-  $injections = array('(\n+)',
-              '(\r+)',
-              '(\t+)',
-              '(%0A+)',
-              '(%0D+)',
-              '(%08+)',
-              '(%09+)'
-              );
-  $inject = join('|', $injections);
-  $inject = "/$inject/i";
-  if(preg_match($inject,$str))
+    /* The response given by the form being submitted */
+    $response_key = $_POST['g-recaptcha-response'];
+    /* Send the data to the API for a response */
+    $response = file_get_contents($url . '?secret=' . $private_key . '&response=' . $response_key);
+    /* json decode the response to an object */
+    $response = json_decode($response);
+    /* if success */
+    if ($response->success == 1)
     {
-    return true;
-  }
-  else
+        $email_from = "$visitor_email"; //<== update the email address
+        $email_subject = "New website submission";
+        $email_body = "You have received a new message from $name.\n" . "Here is the message: $message";
+
+        $to = "admin@davidfagan.co.uk"; //<== update the email address
+        $headers = "From: $email_from \r\n";
+        $headers .= "Reply-To: $visitor_email \r\n";
+        //Send the email!
+        mail($to, $email_subject, $email_body, $headers);
+        //done. redirect to thank-you page.
+        header('Location: thank-you.html');
+    }
+    else
     {
-    return false;
-  }
+        sleep(3);
+        header('Location: captcha.html');
+    }
 }
-   
-?> 
+?>
